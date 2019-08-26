934e475d7a0da1ad10c82de18ec2e534d75e9679
Matthew Hughson <contact@matthughson.com>
2019-08-26 13:09:11 -0700

    initial import from 8bitworkshop.com

---
+++ attributes.c
@@ -0,0 +1,53 @@
+/*
+Setting the attribute table, which controls palette selection
+for the nametable. We copy it from an array in ROM to video RAM.
+*/
+#include "neslib.h"
+#include <string.h>
+#include <stdlib.h>
+
+// link the pattern table into CHR ROM
+//#link "chr_generic.s"
+
+// attribute table in PRG ROM
+// 64 bytes
+const char ATTRIBUTE_TABLE[0x40] = {
+  0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, // rows 0-3
+  0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, // rows 4-7
+  0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, // rows 8-11
+  0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, // rows 12-15
+  0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, // rows 16-19
+  0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, // rows 20-23
+  0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, // rows 24-27
+  0x00, 0x55, 0x00, 0x55, 0x00, 0x55, 0x00, 0x55  // rows 28-29
+};
+
+/*{pal:"nes",layout:"nes"}*/
+const char PALETTE[16] = { 
+  0x03,			// screen color
+
+  0x11,0x30,0x27,0x0,	// background palette 0
+  0x1c,0x20,0x2c,0x0,	// background palette 1
+  0x00,0x10,0x20,0x0,	// background palette 2
+  0x06,0x16,0x26        // background palette 3
+};
+
+// main function, run after console reset
+void main(void) {
+  // set background palette colors
+  pal_bg(PALETTE);
+  
+  // fill nametable with diamonds
+  vram_adr(NAMETABLE_A);	// start address ($2000)
+  // 0x16 = 22
+  vram_fill(0x16, 32*30);	// fill nametable (960 bytes)
+  
+  // copy attribute table from PRG ROM to VRAM
+  vram_write(ATTRIBUTE_TABLE, sizeof(ATTRIBUTE_TABLE));
+
+  // enable PPU rendering (turn on screen)
+  ppu_on_all();
+
+  // infinite loop
+  while (1) { }
+}
